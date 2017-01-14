6761cb3283dbdbaba179c88b54b9de5b53815edf
talyariel <talyariel805@gmail.com>
2017-01-14 22:04:20 +0200

    last changes

--- maps.js
+++ maps.js
@@ -6,7 +6,7 @@ function initialize() {
   geocoder = new google.maps.Geocoder();
   var latlng = new google.maps.LatLng(31.774464, 35.207478);
   var mapOptions = {
-    zoom: 8,
+    zoom: 7,
     center: latlng
   }
   map = new google.maps.Map(document.getElementById('myMap'), mapOptions);
@@ -16,11 +16,11 @@ function MyFunction(address) {
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == 'OK') {
       map.setCenter(results[0].geometry.location);
-      var marker = new google.maps
-      .Marker({
+      var marker = new google.maps.Marker({
           map: map,
           position: results[0].geometry.location
       });
+	   map.setZoom(9);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
