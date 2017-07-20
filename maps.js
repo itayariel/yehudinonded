89f2bd71fa115bd2bdedea03733cf3954ca77985
talyariel <talyariel805@gmail.com>
2017-07-20 09:16:17 +0300

    first commit‏

--- maps.js
+++ maps.js
@@ -6,6 +6,7 @@ var address;
 var marCount;
 var infowindow;
 var city;
+
 /*
 *
 markers data
@@ -52,10 +53,11 @@ function setMikvehPoint(dest){// adding the Mikveh markers
 			zIndex: i 
 		});
 
-		infowindow[marCount] = new google.maps.InfoWindow({
+		
+		pointMarker[marCount].addListener('click', function() {
+            infowindow[marCount] = new google.maps.InfoWindow({
 			content: city[dest].mikvaot[i].desciption
 		});
-		pointMarker[marCount].addListener('click', function() {
 		//infowindow.setContent(this.html);
 		infowindow[marCount].open(map, pointMarker[marCount]);
 		});
@@ -74,10 +76,11 @@ function setSynagoguePoint(dest){// adding the Synagogue markers
 			visible: true,
 			zIndex: i 	
 		});
-		var infowindow = new google.maps.InfoWindow({
+		
+		pointMarker[marCount].addListener('click', function() {
+            var infowindow = new google.maps.InfoWindow({
 			content: city[dest].Synagogues[i].desciption
 		});
-		pointMarker[marCount].addListener('click', function() {
 		infowindow.open(map, pointMarker[marCount]);
 		});
 		marCount++;
@@ -95,10 +98,11 @@ function setRestaurantPoint(dest){// adding the restaurant markers
 			visible: true,
 			zIndex: i 	
 		});
-		var infowindow = new google.maps.InfoWindow({
+		
+		pointMarker[marCount].addListener('click', function() {
+            var infowindow = new google.maps.InfoWindow({
 			content: city[dest].resteraunts[i].desciption
 		});
-		pointMarker[marCount].addListener('click', function() {
 		infowindow.open(map, pointMarker[marCount]);
 		});
 		marCount++;
@@ -116,10 +120,11 @@ function setChabadPoint(dest){// adding the chabad house markers
 			visible: true,
 			zIndex: i 	
 		});
-		var infowindow = new google.maps.InfoWindow({
-			content: city[dest].chabad[i].desciption
-		});
+	
 		pointMarker[marCount].addListener('click', function() {
+            var infowindow = new google.maps.InfoWindow({
+                content: city[dest].chabad[i].desciption
+		});
 		infowindow.open(map, pointMarker[marCount]);
 		});
 		marCount++;
@@ -137,11 +142,12 @@ function setJewishPoint(dest){// adding the jewish house markers
 			visible: true,
 			zIndex: i 	
 		});
-		var infowindow = new google.maps.InfoWindow({
-			content: city[dest].houses[i].desciption
-		});
+		
 		pointMarker[marCount].addListener('click', function() {
-		infowindow.open(map, pointMarker[marCount]);
+            var infowindow = new google.maps.InfoWindow({
+                content: city[dest].houses[i].desciption
+            });
+            infowindow.open(map, pointMarker[marCount]);
 		});
 		marCount++;
 	}
@@ -179,6 +185,118 @@ function clean_null_markers(){
 }
 
 function initialize() { // initialize the map on load 
+   var styledMapType = new google.maps.StyledMapType(
+            [
+              {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+              {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+              {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+              {
+                featureType: 'administrative',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#c9b2a6'}]
+              },
+              {
+                featureType: 'administrative.land_parcel',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#dcd2be'}]
+              },
+              {
+                featureType: 'administrative.land_parcel',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#ae9e90'}]
+              },
+              {
+                featureType: 'landscape.natural',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+              },
+              {
+                featureType: 'poi',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+              },
+              {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#93817c'}]
+              },
+              {
+                featureType: 'poi.park',
+                elementType: 'geometry.fill',
+                stylers: [{color: '#a5b076'}]
+              },
+              {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#447530'}]
+              },
+              {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#f5f1e6'}]
+              },
+              {
+                featureType: 'road.arterial',
+                elementType: 'geometry',
+                stylers: [{color: '#fdfcf8'}]
+              },
+              {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#f8c967'}]
+              },
+              {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#e9bc62'}]
+              },
+              {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry',
+                stylers: [{color: '#e98d58'}]
+              },
+              {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#db8555'}]
+              },
+              {
+                featureType: 'road.local',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#806b63'}]
+              },
+              {
+                featureType: 'transit.line',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+              },
+              {
+                featureType: 'transit.line',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#8f7d77'}]
+              },
+              {
+                featureType: 'transit.line',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#ebe3cd'}]
+              },
+              {
+                featureType: 'transit.station',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+              },
+              {
+                featureType: 'water',
+                elementType: 'geometry.fill',
+                stylers: [{color: '#b9d3c2'}]
+              },
+              {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#92998d'}]
+              }
+            ],
+            {name: 'Styled Map'});
   directionsDisplay = new google.maps.DirectionsRenderer();
   geocoder = new google.maps.Geocoder();
   var latlng = new google.maps.LatLng(31.774464, 35.207478);
@@ -187,10 +305,18 @@ function initialize() { // initialize the map on load
     center: latlng
   }
   map = new google.maps.Map(document.getElementById('myMap'), mapOptions);
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
   var input = document.getElementById('searchTextField');
     //this directionsDisplay will be used to show the route of the requested directions
   directionsDisplay.setMap(map);
   // This event listener will call addMarker() when the map is clicked.
+google.maps.event.addListener(map, "click", function(event) {
+    var lat = event.latLng.lat();
+    var lng = event.latLng.lng();
+    // populate yor box/field with lat, lng
+    alert("Lat=" + lat + "; Lng=" + lng);
+});
 }
 //google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -298,8 +424,7 @@ function calcRoute() {
 }
 //add a data
 $(document).ready(function(){
-	$('#add-location').on('submit', function(e){
-		e.preventDefult();
+	$('#add-location').on('click', function(e){
 		var Type = $('#Type').val();
 		var Address = $('#Address').val();
 		var Description = $('#Description').val();
@@ -316,7 +441,7 @@ $.ajax({ url: "https://api.mlab.com/api/1/databases/mydata/collections/city?apiK
 			"description": Description
 		  }),
 		  type: "POST",
-		  contentType: "application/json" 
+		  contentType: "application/json", 
 		  success: function(data){
 			window.location.href = "map.html"
 		  },
@@ -326,3 +451,4 @@ $.ajax({ url: "https://api.mlab.com/api/1/databases/mydata/collections/city?apiK
 		 });
 	});
 });
+
